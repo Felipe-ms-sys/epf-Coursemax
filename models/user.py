36883fd848e9908688d6f9bc26c22a7c9f0980ce
Felipe Melo <felipems.ctt@gmail.com>
2025-12-01 16:08:26 -0300

    Personalização da model user com a criação de usuários

--- models/user.py
+++ models/user.py
@@ -1,31 +1,27 @@
 import json
 import os
-from dataclasses import dataclass, asdict
-from typing import List
+import hashlib
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), '..', 'data')
 
 class User:
-    def __init__(self, id, name, email, birthdate):
+    DOMINIOS_EMAIL = ['gmail.com', 'hotmail.com', 'outlook.com', 'yahoo.com']
+
+    def __init__(self, id, name, email, cpf, password):
         self.id = id
         self.name = name
         self.email = email
-        self.birthdate = birthdate
-
-
-    def __repr__(self):
-        return (f"User(id={self.id}, name='{self.name}', email='{self.email}', "
-                f"birthdate='{self.birthdate}'")
-
+        self.cpf = cpf
+        self.password = password
 
     def to_dict(self):
         return {
             'id': self.id,
             'name': self.name,
             'email': self.email,
-            'birthdate': self.birthdate
+            'cpf': self.cpf,
+            'password': self.password
         }
-
 
     @classmethod
     def from_dict(cls, data):
@@ -33,7 +29,8 @@ class User:
             id=data['id'],
             name=data['name'],
             email=data['email'],
-            birthdate=data['birthdate']
+            cpf=data['cpf'],
+            password=data.get('password', '')
         )
 
 
@@ -43,7 +40,6 @@ class UserModel:
     def __init__(self):
         self.users = self._load()
 
-
     def _load(self):
         if not os.path.exists(self.FILE_PATH):
             return []
@@ -51,33 +47,27 @@ class UserModel:
             data = json.load(f)
             return [User(**item) for item in data]
 
-
     def _save(self):
         with open(self.FILE_PATH, 'w', encoding='utf-8') as f:
             json.dump([u.to_dict() for u in self.users], f, indent=4, ensure_ascii=False)
 
-
     def get_all(self):
         return self.users
 
-
-    def get_by_id(self, user_id: int):
+    def get_by_id(self, user_id):
         return next((u for u in self.users if u.id == user_id), None)
 
-
-    def add_user(self, user: User):
+    def add_user(self, user):
         self.users.append(user)
         self._save()
 
-
-    def update_user(self, updated_user: User):
+    def update_user(self, updated_user):
         for i, user in enumerate(self.users):
             if user.id == updated_user.id:
                 self.users[i] = updated_user
                 self._save()
                 break
 
-
-    def delete_user(self, user_id: int):
+    def delete_user(self, user_id):
         self.users = [u for u in self.users if u.id != user_id]
         self._save()
