5454f105c45fd3433b780eaf15d5e7dff432d1cf
G2SBiell <gabrielportaciocandeiacosta@gmail.com>
2025-12-02 22:52:05 -0300

    Mudanças no controller diciplina

--- controllers/disciplina_controller.py
+++ controllers/disciplina_controller.py
@@ -2,11 +2,13 @@ import json
 from bottle import Bottle, request, response
 from .base_controller import BaseController
 from services.disciplina_service import DisciplinaService
+from services.user_service import UserService  
 
 class DisciplinaController(BaseController):
     def __init__(self, app):
         super().__init__(app)
         self.disciplina_service = DisciplinaService()
+        self.user_service = UserService()
         self.setup_routes()
 
     def setup_routes(self):
@@ -16,22 +18,38 @@ class DisciplinaController(BaseController):
 
     def dashboard(self):
         user_id = request.get_cookie("user_id", secret='sua-chave-secreta-aqui')
+        
         if not user_id:
             self.redirect('/login')
             return
+
+        usuario = self.user_service.user_model.get_by_id(int(user_id))
+        nome_real = usuario.name if usuario else "Estudante"
 
         disciplinas_objs = self.disciplina_service.listar_por_usuario(int(user_id))
         disciplinas_dicts = [d.to_dict() for d in disciplinas_objs]
 
         return self.render('dashboard', 
                            disciplines_json=json.dumps(disciplinas_dicts), 
-                           user_name="Estudante")
+                           user_name=nome_real)
 
     def adicionar(self):
-        user_id = request.get_cookie("user_id", secret='sua-chave-secreta-aqui') or 1
+        user_id = request.get_cookie("user_id", secret='sua-chave-secreta-aqui')
+        if not user_id:
+            self.redirect('/login')
+            return
+
         nome = request.forms.get('disciplineName')
         color = request.forms.get('disciplineColor')
-        self.disciplina_service.criar_disciplina(int(user_id), nome, 60, 2, 'Padrao')
+        
+        self.disciplina_service.criar_disciplina(
+            id_usuario=int(user_id), 
+            nome=nome, 
+            color=color, 
+            horas=60, 
+            num_provas=2, 
+            metodo='Padrao'
+        )
         self.redirect('/dashboard')
 
     def remover(self, id):
