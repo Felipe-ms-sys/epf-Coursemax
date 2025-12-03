8b61ff5d8aba0cf8cd12e220525711bfd30aec07
Felipe Melo <felipems.ctt@gmail.com>
2025-12-03 00:04:51 -0300

    Pequenas modificações no nome das funções

--- services/configuracoes_service.py
+++ services/configuracoes_service.py
@@ -1,20 +1,20 @@
-from models.usuario import UsuarioModel
+from models.user import UserModel
 
 class ConfiguracaoService:
     def __init__(self):
-        self.usuario_model = UsuarioModel()
+        self.user_model = UserModel()
 
     def editar_usuario(self, usuario_atualizado):
-        usuario_existente = self.usuario_model.buscar_por_id(usuario_atualizado.id)
+        usuario_existente = self.user_model.get_by_id(usuario_atualizado.id)
         if not usuario_existente:
             raise ValueError("Usuário não encontrado para edição")
         
-        self.usuario_model.atualizar(usuario_atualizado)
+        self.user_model.update_user(usuario_atualizado)
         return usuario_existente
 
     def excluir_usuario(self, id_usuario):
-        usuario = self.usuario_model.buscar_por_id(id_usuario)
+        usuario = self.user_model.get_by_id(id_usuario)
         if not usuario:
             raise ValueError("Usuário não encontrado para exclusão")
 
-        self.usuario_model.remover(id_usuario)
+        self.user_model.delete_user(id_usuario)
